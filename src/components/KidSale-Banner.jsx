d0db08e6bedc1f79d15cfd8ea04f4ca1634516a1
fakhiriqbal <fakhiriqbal52@gmail.com>
2024-06-01 21:56:35 +0500

    Add Some Changes

--- src/components/KidSale-Banner.jsx
+++ src/components/KidSale-Banner.jsx
@@ -1,8 +1,8 @@
 import "./style.css";
-import {useEffect } from "react";
+import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Kid_img from "../assest/image 10.png";
+import banner from "../assest/salebanner.png";
 import { Container, Grid } from "@mui/material";
 import Button from "@mui/material/Button";
 
@@ -19,20 +19,19 @@ export default function KidBanner() {
             item
             xs={12}
             md={10}
-            className="flex overflow-hidden h-[320px] bg-[#e8f8f6] text-white rounded-2xl"
+            className="flex overflow-hidden h-[320px] rounded-2xl background-image text-white"
           >
-            <div className="p-4 w-1/2 flex flex-col justify-center items-center text-center">
-              <h1 className="text-3xl font-thin text-black md:text-5xl">
-                Baby & Kid's Fashion
+            <div className="p-4 ml-8 w-full md:w-1/2 flex flex-col justify-center">
+              <h1 className="text-5xl font-black md:text-black md:text-6xl">
+                Flash Sale
               </h1>
-              <div className="flex flex-col items-center mt-3">
-                <div className="text-white z-10 mr-9 md:mr-14">
-                  <h1 className="font-bold text-black">SALE!</h1>
-                  <p className="text-[#004743] text-sm font-semibold mt-0">
-                    07 to 14 February
-                  </p>
-                </div>
-              </div>
+              <h1 className="md:text-[#004743] text-2xl md:text-3xl italic font-semibold mt-0">
+                Get Discount <span className="text-[#fbd103]">30%</span> Off
+              </h1>
+              <p className="italic text-sm md:text-black">
+                Enjoy a generous 30% discount voucher on your upcoming purchase
+                when you spend a minimum of $100.
+              </p>
 
               <Button
                 variant="contained"
@@ -42,8 +41,12 @@ export default function KidBanner() {
               </Button>
             </div>
 
-            <div className="overflow-hidden w-1/2 flex justify-center">
-              <img src={Kid_img} alt="" className="h-60 object-cover rounded md:h-72 mt-8" />
+            <div className="overflow-hidden w-1/2 hidden md:block">
+              <img
+                src={banner}
+                alt=""
+                className="h-60 object-cover ml-[100px] rounded md:h-72 mt-8"
+              />
             </div>
           </Grid>
         </Grid>
